c8b4c674d1a9eae2edba02426361f2a21a094ff7
alexandramikheyeva <alexandramikheeva@mail.ru>
2023-04-09 07:23:20 +0300

    4 commit

--- homework.py
+++ homework.py
@@ -10,3 +10,11 @@ if a > b + c:
     print(a - b - c)
 else:
     print('False')
+#4
+a = int(input('Введите число:'))
+b = int(input('Введите число:'))
+c = int(input('Введите число:'))
+if a < b + c:
+    print(b + c - a)
+else:
+    print('False')
