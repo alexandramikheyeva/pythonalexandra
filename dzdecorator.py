3a7510f500970699e67d64c4bc62525a7a1b8149
alexandramikheyeva <alexandramikheeva@mail.ru>
2023-04-11 23:51:19 +0300

    commit

--- dzdecorator.py
+++ dzdecorator.py
@@ -1,8 +1,13 @@
 def decorator(func):
-
     def wrapper_decorator(*args, **kwargs):
-
-        value = func(*args, **kwargs)
-
+        value = f'сумма счета:{func(*args, **kwargs)}'
         return value
     return wrapper_decorator
+@decorator
+def adds():
+    return 2
+login = input('Username:')
+if login == 'admin':
+    print(adds())
+else:
+    print('Доступ запрещен')
