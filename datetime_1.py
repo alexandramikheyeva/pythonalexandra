2b61db500ddb50a9020c67fa4c50ea6df5acc9b3
alexandramikheyeva <alexandramikheeva@mail.ru>
2023-04-26 22:02:35 +0300

    commit 11

--- datetime_1.py
+++ datetime_1.py
@@ -3,106 +3,110 @@ import os
 current_datetime = datetime.now()
 print(f'{current_datetime.hour}:{current_datetime.minute}:{current_datetime.second}')
 
-zero = """
+output_number=\
+{0:\
+  """
    ■■■■  
  ■■    ■■ 
 ■■      ■■
 ■■      ■■
  ■■    ■■ 
    ■■■■  
-"""
-
-one = """
-    ■■■   
- ■■■■■■   
-    ■■■   
-    ■■■   
-    ■■■   
- ■■■■■■■■
-"""
-
-two = """
-  ■■■■■■  
-■■    ■■■
-     ■■■
-   ■■■   
- ■■■    
-■■■■■■■■■
-"""
-
-three = """
+  """,
+1:\
+  """
 ■■■■■■ 
      ■■■
 ■■■■■■ 
      ■■■
      ■■■
 ■■■■■■
-"""
+""",
 
-four = """
+
+2:\
+"""
+  ■■■■■■  
+■■    ■■■
+     ■■■
+   ■■■   
+ ■■■    
+■■■■■■■■■
+""",
+3:\
+  """
+■■■■■■ 
+     ■■■
+■■■■■■ 
+     ■■■
+     ■■■
+■■■■■■
+""",
+4:\
+  """
 ■■      ■■
 ■■      ■■
 ■■      ■■
 ■■■■■■■■■■
         ■■
         ■■
-"""
-
-five = """
+""",
+5:\
+  """
 ■■■■■■■■■
 ■■        
 ■■■■■■■■ 
         ■■
         ■■
 ■■■■■■■■ 
-"""
-
-six = """
+""",
+6:\
+  """
 ■■■■■■■■
 ■■        
 ■■■■■■■■
 ■■     ■■
 ■■     ■■
  ■■■■■■■ 
-"""
-
-seven = """
+""",
+7:\
+  """
  ■■■■■■■■■
         ■■
       ■■ 
     ■■   
     ■■   
     ■■  
-"""
-
-eight = """
+""",
+8:\
+  """
   ■■■■■■ 
 ■■      ■■
   ■■■■■■ 
 ■■      ■■
 ■■      ■■
   ■■■■■■ 
-"""
-
-nine = """
+""",
+9:\
+  """
   ■■■■■■ 
 ■■      ■■
 ■■      ■■
   ■■■■■■■■
         ■■
    ■■■■■■■
+""",
+'dot':\
 """
-
-dot = """
     ■■    
     ■■    
           
           
     ■■    
     ■■    
+""",
+'nothing':\
 """
-
-nothing = """
           
           
           
@@ -110,22 +114,44 @@ nothing = """
           
           
 """
-
-output_number = {
-    "0": zero,
-    "1": one,
-    "2": two,
-    "3": three,
-    "4": four,
-    "5": five,
-    "6": six,
-    "7": seven,
-    "8": eight,
-    "9": nine,
-    ":": dot,
-    "empty": nothing,
 }
 def clear_screen():
-    os.system(clear_screen)
+  os.system(clear_screen)
 
-def new_hours():    
+def print_result(hour_1, hour_2, minute_1, minute_2, second_1, second_2):
+  line_1 = hour_1.splitlines()
+  line_2 = hour_2.splitlines()
+  line_3 = minute_1.splitlines()
+  line_4 = minute_2.splitlines()
+  line_5 = second_1.splitlines()
+  line_6 = second_2.splitlines()
+  dot = output_number['dot']
+  dot_new = dot.splitlines()
+  nothing = output_number['nothing']
+  nothing_new = nothing.splitlines()
+  if datetime.now().second % 2 == 0:
+    for i in range (len(line_1)):
+      print(f'{line_1[i]}, {line_2[i]}, {nothing_new[i]}, {line_3[i]}, {line_4[i]}, \
+            {nothing_new[i]}, {line_5[i]}, {line_6[i]}')
+  elif datetime.now().second % 2 != 0:
+    for i in range (len(line_1)):
+      print(f'{line_1[i]}, {line_2[i]}, {dot_new[i]}, {line_3[i]}, {line_4[i]}, \
+            {dot_new[i]}, {line_5[i]}, {line_6[i]}')
+  
+  def null_check():
+
+
+
+
+  def new_second(today_time):
+    split_second = [int(a) for a in str(today_time.second)]
+    new_split_second = null_check(split_second)
+    new_split_second_1, new_split_second_2 = output_number[new_split_second[0]], \
+      output_number[new_split_second[1]]
+    return new_split_second_1, new_split_second_2
+                                          
+
+
+
+
+
--- numbers/datetime_1.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import os
-from numbers import numbers
-now = datetime.now()
-current_time = now.strftime('%H:%M:%S')
-print('Current time =', current_time)
-def clear_screen():
-    os.system('clear')
-    
