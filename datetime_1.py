db46c2a6b6e7420ca4c5758803ff4fd207225899
alexandramikheyeva <alexandramikheeva@mail.ru>
2023-04-26 22:15:53 +0300

    commit 15

--- datetime_1.py
+++ datetime_1.py
@@ -138,17 +138,29 @@ def print_result(hour_1, hour_2, minute_1, minute_2, second_1, second_2):
       print(f'{line_1[i]}, {line_2[i]}, {dot_new[i]}, {line_3[i]}, {line_4[i]}, \
             {dot_new[i]}, {line_5[i]}, {line_6[i]}')
   
-  def null_check():
+def null_check():
+  check_lenght = len(str())
 
+def new_hour(today_time):
+  split_hour = [int(a) for a in str(today_time.hour)]
+  new_split_hour = null_check(split_hour)
+  new_split_hour_1, new_split_hour_2 = output_number[new_split_hour[0]], \
+  output_number[new_split_hour[1]]
+  return new_split_hour_1, new_split_hour_2
 
+def new_minute(today_time):
+  split_minute = [int(a) for a in str(today_time.minute)]
+  new_split_minute = null_check(split_minute)
+  new_split_minute_1, new_split_minute_2 = output_number[new_split_minute[0]], \
+  output_number[new_split_minute[1]]
+  return new_split_minute_1, new_split_minute_2
 
-
-  def new_second(today_time):
-    split_second = [int(a) for a in str(today_time.second)]
-    new_split_second = null_check(split_second)
-    new_split_second_1, new_split_second_2 = output_number[new_split_second[0]], \
-      output_number[new_split_second[1]]
-    return new_split_second_1, new_split_second_2
+def new_second(today_time):
+  split_second = [int(a) for a in str(today_time.second)]
+  new_split_second = null_check(split_second)
+  new_split_second_1, new_split_second_2 = output_number[new_split_second[0]], \
+  output_number[new_split_second[1]]
+  return new_split_second_1, new_split_second_2
                                           
 
 
